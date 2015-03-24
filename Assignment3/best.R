b7e950ef9f16642e5aec0a7f8a3568ffc504d98b
Steve Tannenbaum <charlietuna49@yahoo.com>
2015-03-23 23:57:36 -0700

    corrected pneumonia data

--- Assignment3/best.R
+++ Assignment3/best.R
@@ -5,11 +5,11 @@ best <- function(state, outcome){
   ## Read outcome data
   ## elements are the columns needed for analysis
   ## Hosp, State, MI, CHF, Pneumonia
-  elements <- c(2,7,11,19,27)
+  elements <- c(2,7,11,19,23)
   outcomeOfCare <- read.csv("C:/Users/Charlietuna/datasciencecoursera/Assignment3/outcome-of-care-measures.csv", stringsAsFactors=FALSE)
   states <- unique(HospStateOutcomes["State"])## list of unique states
   HospStateOutcomes <- outcomeOfCare[,elements] ## only columns needed
-  HospStateOutcomes<-head(HospStateOutcomes)
+  ##HospStateOutcomes<-head(HospStateOutcomes)
  
   name<-names(HospStateOutcomes)<- c("Hosp", "State", "heart attack", "heart failure", "pneumonia")#rename the columns
   outComeName<-c("heart attack","heart failure","pneumonia")
@@ -21,17 +21,19 @@ best <- function(state, outcome){
   ## Check that state and outcome are valid
   if(sum(outcome == outComeName)!= 1)stop("invalid outcome")
   if(sum(state==states)!= 1) stop("invalid state")
-  
+  ##t<-(HospStateOutcomes[,"State"])==state
+  HospStateOutcomes<-HospStateOutcomes[(HospStateOutcomes[,"State"])==state,]
   ##print(HospStateOutcomes)
   ## change "Not Available" to NA
   HospStateOutcomes[(HospStateOutcomes[,outcome]=="Not Available"),outcome]<-NA
   ## change outcome column to numeric prior to sorting
   HospStateOutcomes[,outcome]<-as.numeric(HospStateOutcomes[,outcome])
+  ##print(HospStateOutcomes)
   
   ## sort and eliminate NA data
   sortOrder<-order(HospStateOutcomes[,outcome],HospStateOutcomes[,"Hosp"],na.last=NA)
   HospStateOutcomes<-HospStateOutcomes[sortOrder,]
-  print(HospStateOutcomes)
+  ##print(HospStateOutcomes)
   
   ## Return hospital name in that state with lowest 30- day death
   ## rate
