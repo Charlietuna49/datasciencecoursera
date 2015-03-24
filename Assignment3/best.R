8070ecfa7bb9f3b3bf9778b1bdb43c2e75a85d4c
Steve Tannenbaum <charlietuna49@yahoo.com>
2015-03-23 23:05:05 -0700

    working copy

--- Assignment3/best.R
+++ Assignment3/best.R
@@ -9,20 +9,34 @@ best <- function(state, outcome){
   outcomeOfCare <- read.csv("C:/Users/Charlietuna/datasciencecoursera/Assignment3/outcome-of-care-measures.csv", stringsAsFactors=FALSE)
   states <- unique(HospStateOutcomes["State"])## list of unique states
   HospStateOutcomes <- outcomeOfCare[,elements] ## only columns needed
-  name<-names(HospStateOutcomes)<- c("Hosp", "State", "MI", "CHF", "Pneumonia")#rename the columns
+  HospStateOutcomes<-head(HospStateOutcomes)
+ 
+  name<-names(HospStateOutcomes)<- c("Hosp", "State", "heart attack", "heart failure", "pneumonia")#rename the columns
+  outComeName<-c("heart attack","heart failure","pneumonia")
+  ##print(HospStateOutcomes)
   
   ##------------------------------------------------------------------------------------
   
   
   ## Check that state and outcome are valid
-  if(sum(outcome == name)!= 1)stop("invalid outcome")
+  if(sum(outcome == outComeName)!= 1)stop("invalid outcome")
   if(sum(state==states)!= 1) stop("invalid state")
   
+  ##print(HospStateOutcomes)
+  ## change "Not Available" to NA
+  HospStateOutcomes[(HospStateOutcomes[,outcome]=="Not Available"),outcome]<-NA
+  ## change outcome column to numeric prior to sorting
+  HospStateOutcomes[,outcome]<-as.numeric(HospStateOutcomes[,outcome])
   
-  
+  ## sort and eliminate NA data
+  sortOrder<-order(HospStateOutcomes[,outcome],HospStateOutcomes[,"Hosp"],na.last=NA)
+  HospStateOutcomes<-HospStateOutcomes[sortOrder,]
+  print(HospStateOutcomes)
   
   ## Return hospital name in that state with lowest 30- day death
   ## rate
+  HospStateOutcomes[1,1]
+  ##
   
   
   
