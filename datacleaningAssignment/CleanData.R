851d5dce87fbc36027991a94a6c64a02b0688a7c
Steve Tannenbaum <charlietuna49@yahoo.com>
2015-04-20 22:05:46 -0700

    fixed meanstd removed -,bodybody, and ()

--- datacleaningAssignment/CleanData.R
+++ datacleaningAssignment/CleanData.R
@@ -5,6 +5,8 @@ nrow(features)
 if(!exists("X_test")) X_test <- read.table("C:/Users/Charlietuna/datasciencecoursera/datacleaningAssignment/test/X_test.txt", quote="\"", stringsAsFactors=FALSE)
 ##View(X_test)
 ##ncol(X_test)
+if(!exists(X_train)) X_train <-X_train <- read.table("C:/Users/Charlietuna/datasciencecoursera/datacleaningAssignment/train/X_train.txt", quote="\"", stringsAsFactors=FALSE)
+
 
 std<-features[grep("std",features$V2),]
 mean<-features[grep("mean\\(\\)",features$V2),]
@@ -15,7 +17,11 @@ meanStd <- c(mean$V1, std$V1)
 
 dfColumns<- X_test %>%
   select(meanStd)
+dfColumnsTrain <-X_train %>%
+  select(meanStd)
+
 ##print(head(dfColumns))
 nme<- c(mean$V2,std$V2)
 names(dfColumns)<-nme
+names(dfColumnsTrain)<-nme
 print(head(dfColumns))
